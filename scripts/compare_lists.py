4aa75382a464284fcc37b593991bc45778942369
Nae-Chyun <s93rm6@gmail.com>
2020-03-18 14:51:11 -0400

    added argparse

--- scripts/compare_lists.py
+++ scripts/compare_lists.py
@@ -1,30 +1,57 @@
-gene = 'TRBV'
+import argparse
 
-list_blastn = []
-# with open('trbv_top50.txt', 'r') as f:
-with open('trbv_top80.txt', 'r') as f:
-    for line in f:
-        line = line.strip()
-        if line.count(gene) == 0:
-            print ('warning:', line)
-        else:
-            list_blastn.append(line)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-fc', '--fn_called',
+        help = 'called allele list'
+    )
+    parser.add_argument(
+        '-fa', '--fn_annotated',
+        help = 'annotated allele list'
+    )
+    parser.add_argument(
+        '-g', '--gene',
+        help = 'gene name of interest'
+    )
+    args = parser.parse_args()
+    return args
 
-list_annotated = []
-with open('trbv_all.txt', 'r') as f:
-    for line in f:
-        line = line.rstrip()
-        if line.count(gene) == 0:
-            print ('warning:', line)
-        else:
-            list_annotated.append(line)
+def compare_files(fn_called, fn_annotated, gene):
+    # gene = 'TRBV'
+    list_blastn = []
+    with open(fn_called, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.count(gene) == 0:
+                print ('warning:', line)
+            else:
+                list_blastn.append(line)
 
-set_blastn = set(list_blastn)
-set_annotated = set(list_annotated)
+    list_annotated = []
+    with open(fn_annotated, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            if line.count(gene) == 0:
+                print ('warning:', line)
+            else:
+                list_annotated.append(line)
 
-print ('Size of blastn alleles:', len(set_blastn))
-print ('Size of annotated alleles:', len(set_annotated))
-print ('Size of intersection:', len(set_blastn.intersection(set_annotated)))
-print ('Size of union:', len(set_blastn.union(set_annotated)))
+    set_blastn = set(list_blastn)
+    set_annotated = set(list_annotated)
 
-print ('Found by blastn, not annotated:', set_blastn - set_blastn.intersection(set_annotated))
+    print ('Size of blastn alleles:', len(set_blastn))
+    print ('Size of annotated alleles:', len(set_annotated))
+    print ('Size of intersection:', len(set_blastn.intersection(set_annotated)))
+    print ('Size of union:', len(set_blastn.union(set_annotated)))
+
+    print ('Found by blastn, not annotated:', set_blastn - set_blastn.intersection(set_annotated))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    fn_called = args.fn_called
+    fn_annotated = args.fn_annotated
+    gene = args.gene
+
+    compare_files(fn_called, fn_annotated, gene)
