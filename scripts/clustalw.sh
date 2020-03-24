9cb06ac337b26de2476ab1f6c5d10dc934b4f100
maoandchiang <r05943039@ntu.edu.tw>
2020-03-24 15:44:55 +0800

    add the plot tree command

--- scripts/clustalw.sh
+++ scripts/clustalw.sh
@@ -3,4 +3,7 @@
 # should produce alignment .aln file and tree .dnd file in the target directory
 
 # parse .dnd file
-python3 ../script/parse_tree.py -ft ./experiment/TRBV_filtered.dnd -o ./experiment/TRBV_parsed_tree.dnd
+python3 ../immunogenomics/script/parse_tree.py -ft ./experiment/TRBV_filtered.dnd -o ./experiment/TRBV_parsed_tree.dnd
+
+# plot tree
+Rscript ../immunogenomics/script/plot_tree.r -t ./experiment/TRBV_parsed_tree.dnd -a ./experiment/trbv_top80.labelled.txt -s 1 -o ./experiment/tree.pdf
