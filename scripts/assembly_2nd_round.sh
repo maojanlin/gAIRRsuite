1086e4311c481b74b160906224788191f3146ffe
maoandchiang <r05943039@ntu.edu.tw>
2020-07-31 00:42:41 +0800

    Update the usage of parse_contig_realigh.py
    If we rely on the correction path, then the 2nd assembly by SPAdes can be omitted in the future.

--- scripts/assembly_2nd_round.sh
+++ scripts/assembly_2nd_round.sh
@@ -6,7 +6,7 @@ interested_region=$3
 # align reads back to the SPAdes_assembly contigs as "realign.sam"
 ../bwa/bwa mem -t 16 $outer_dir"asm_contigs/TCRV_contigs_"$cluster_id".fasta" $outer_dir"asm_reads_and_alleles/TCRV_"$cluster_id"_H1.fasta" $outer_dir"asm_reads_and_alleles/TCRV_"$cluster_id"_H2.fasta" > $outer_dir"asm_back_align/TCRV_realign_"$cluster_id".sam"
 # parse the "realign.sam", and pop the perfect match to the 1st_round_contigs to generate the alternative remain_P1.fasta and remain_P2.fasta
-python3 parse_contig_realign.py -fs $outer_dir"asm_back_align/TCRV_realign_"$cluster_id".sam" -fo $outer_dir"asm_back_align/TCRV_remain_"$cluster_id".fasta" > $outer_dir"asm_back_align/TCRV_realign_"$cluster_id".rpt" -it_region $interested_region
+python3 parse_contig_realign.py -fs $outer_dir"asm_back_align/TCRV_realign_"$cluster_id".sam" -fo $outer_dir"asm_back_align/TCRV_remain_"$cluster_id".fasta" > $outer_dir"asm_back_align/TCRV_realign_"$cluster_id".rpt" -it_region $interested_region --contig_file $outer_dir"asm_contigs/TCRV_contigs_"$cluster_id".fasta" --allele_file $outer_dir"asm_reads_and_alleles/TCRV_"$cluster_id"_allele.fasta" --corrected_contig_output_file $outer_dir"asm_alignment/corrected_contig_size300.fasta"
 
 ## align remain.fasta back to 1st_round_contig for evaluation
 #../bwa/bwa mem -t 16 $outer_dir"asm_contigs/TCRV_contigs_"$cluster_id".fasta" $outer_dir"asm_back_align/TCRV_remain_"$cluster_id"_P1.fasta" $outer_dir"asm_back_align/TCRV_remain_"$cluster_id"_P2.fasta" > $outer_dir"asm_back_align/align_TCRV_remain_"$cluster_id".sam"
