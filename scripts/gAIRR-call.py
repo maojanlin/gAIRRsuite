8f60a98f25991133903f16581ad91e73ad6d05e6
maojanlin <mlin77@jhu.edu>
2023-09-16 19:59:50 -0400

    adding the intermediate file deletion feature for gAIRR-call module.

--- scripts/gAIRR-call.py
+++ scripts/gAIRR-call.py
@@ -33,6 +33,7 @@ def main():
     parser.add_argument('-rd1', '--read1', help="Path to gAIRR-seq. Pair-end 1", required=True)
     parser.add_argument('-rd2', '--read2', help="Path to gAIRR-seq. Pair-end 2", required=True)
     parser.add_argument('--flanking', help="Option to do flanking sequence analysis.", action='store_true')
+    parser.add_argument('--keep', help="Option specify to keep to sam files", action='store_true')
     parser.add_argument('-t', '--thread', help="Number of threads to use [max].", type=int)
     args = parser.parse_args()
 
@@ -42,6 +43,7 @@ def main():
     path_read1    = args.read1
     path_read2    = args.read2
     flag_flanking = args.flanking
+    flag_keeping  = args.keep
     thread        = args.thread
     if thread == None:
         thread = get_max_thread()
@@ -66,16 +68,26 @@ def main():
         print("[gAIRR-call]", person_name, allele_name, "novel allele calling...")
         command = ' '.join(["bash", path_module+"novel_allele.sh", workspace+'/'+person_name, allele_name, allele_path, person_name, path_read1, path_read2, str(thread)])
         subprocess.call(command, shell=True)
+        if flag_keeping == False:
+            subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_novel/bwa_read_to_allele.sam' , shell=True)
         
         allele_path = workspace+'/'+person_name+'/'+person_name+'_'+allele_name+"_novel/"+allele_name+"_with_novel.fasta"
         print("[gAIRR-call]", person_name, allele_name, "allele calling...")
         command = ' '.join(["bash", path_module+"allele_calling.sh", workspace+'/'+person_name, allele_name, allele_path, person_name, path_read1, path_read2, str(thread)])
         subprocess.call(command, shell=True)
+        if flag_keeping == False:
+            subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'/bwa_read_to_allele_all.sam' , shell=True)
         
         if flag_flanking:
             print("[gAIRR-call]", person_name, allele_name, "flanking sequence calling...")
-            command = ' '.join(["bash", path_module+"allele_calling.sh", workspace+'/'+person_name, allele_name, allele_path, person_name, path_read1, path_read2, 'spades.py', str(thread)])
+            command = ' '.join(["bash", path_module+"flanking_sequence.sh", workspace+'/'+person_name, allele_name, allele_path, person_name, path_read1, path_read2, 'spades.py', str(thread)])
             subprocess.call(command, shell=True)
+            if flag_keeping == False:
+                subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_flanking/asm_check/*.sam' , shell=True)
+                subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_flanking/haplotype_sam/bwa_reads_to_flanking.sam' , shell=True)
+                subprocess.call("rm -rf " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_flanking/asm_contigs/*/' , shell=True)
+                subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_flanking/asm_contigs/*.fasta.*' , shell=True)
+                subprocess.call("rm " + workspace+'/'+person_name+'/'+person_name+'_'+allele_name+'_flanking/group_allele_reads/*.fasta' , shell=True)
 
     
 
